69d560f6d8c1dfaa7769b3599c9c713b495bbe40
KanishkTech <138684623+KanishkTech@users.noreply.github.com>
2025-01-22 11:30:29 +0530

    Update user profile page layout and style Navbar component

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const page = () => {
+  
   return (
-    <div>page</div>
+    <>
+    <div className='bg-white w-full h-screen ' >
+      <div className='justify-center items-center w-full flex'>
+        <h1 className='text-black font-bold text-3xl'>Public Profile Link</h1>
+
+      </div>
+    </div>
+    </>
   )
 }
 
